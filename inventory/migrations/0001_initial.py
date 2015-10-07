7e67cb8c637512b33a5f030852da5f5d11c136b6
Max Theobald <mtheobald@Maxs-MacBook-Pro-6.local>
2015-10-07 16:27:26 -0600

    base.html no existy, but it does

---
+++ inventory/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('parent', models.ForeignKey(blank=True, to='inventory.Category', null=True)),
+            ],
+            options={
+                'verbose_name_plural': 'categories',
+            },
+        ),
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('quantity', models.IntegerField(default=0)),
+                ('sku', models.CharField(max_length=200, null=True, blank=True)),
+                ('category', models.ForeignKey(to='inventory.Category')),
+            ],
+        ),
+    ]
